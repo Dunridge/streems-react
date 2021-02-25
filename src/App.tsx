e9e46e8a93c63f9b543677d62245f3efcadb8745
Dunridge <Polinka7max@gmail.com>
2021-02-25 07:26:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__header">Greetings</h1>
+    </div>
+  );
+}
+
+export default App;
