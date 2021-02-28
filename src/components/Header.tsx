1f11bd3a53292c1bb6c9e49a3595cf5699c0fc89
Dunridge <Polinka7max@gmail.com>
2021-02-28 11:32:20 +0200

    added a searchbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import SearchField from 'react-search-field';
 
 export const Header: React.FC = () => {
+
+    const onChange = () => {
+
+    }
 
     return (
         <div className="header">
             <Link to='/' style={{ textDecoration: 'none' }}>
                 <div className="header__title">Streems test</div>
             </Link>
+            <SearchField placeholder="Search..."
+                         onChange={onChange}
+                         searchText="Find characters..."
+                         classNames="header__search-field"
+            />
         </div>
     );
 }
